7c955d2f99adcdb9090e003f39bace034478fddf
James Johnson <jamesjhnsn1995@gmail.com>
2022-09-30 13:10:40 -0700

    restart

--- blog_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'blog_app'
-urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('create/', views.create, name='create'),
-]
